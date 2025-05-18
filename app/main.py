84e200869a39c62fd9b3efecd971f88a2749912e
ayush <ayush.agarwal@spinny.com>
2025-05-18 15:15:47 +0530

    sending header and coorelation id to client in 4 bytes each

--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    conn, raddr = server.accept() # wait for client
+    # data = conn.recv(32)
+    # print(f"data from client {data.decode()}")
+    # conn.sendall(b"hello from the server side")
+    conn.sendall((0).to_bytes(4, signed=True) + (7).to_bytes(4, signed=True))
+
 
 
 if __name__ == "__main__":
