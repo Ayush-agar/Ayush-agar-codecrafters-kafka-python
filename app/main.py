b21d0f8a86cb6d346133ac40a0964f94acdc1bc3
ayush <ayush.agarwal@spinny.com>
2025-05-18 23:11:54 +0530

    experimenting 3

--- app/main.py
+++ app/main.py
@@ -21,22 +21,20 @@ def main():
         value = int.from_bytes(data[offset : offset + v], byteorder="big")
         req_values[k] = value
         offset += v
-    print(req_values)
+    # print(req_values)
     correlation_id = struct.unpack_from('>I', buffer=data, offset=8)
     tag_buffer = b"\x00"
     # correlation , error code, num_api_keys, api_key, min_version, max_version, tag_buffer, throttle_time_ms, tag_buffer
-    message_bytes = (((((((((req_values['correlation_id']).to_bytes(4, signed=True))
-                     + (0).to_bytes(2, signed=True)) + (1).to_bytes(1, signed=True)
-                     + (18).to_bytes(2, signed=True))
-                     + (0).to_bytes(2, signed=True))
-                     + (4).to_bytes(2, signed=True))
-                     + tag_buffer)
-                     + (0).to_bytes(4, signed=True))
-                     + tag_buffer)
-    req_len = len(message_bytes).to_bytes(4, byteorder="big", signed=True)
+    message_bytes = (req_values['correlation_id']).to_bytes(4, signed=True) + (0).to_bytes(2, signed=True) + (1).to_bytes(1, signed=True) + (18).to_bytes(2, signed=True) + (0).to_bytes(2, signed=True) + (4).to_bytes(2, signed=True) + tag_buffer + (0).to_bytes(4, signed=True) + tag_buffer
+    # print(REQUEST_SIZE)
+    # print(message_bytes)
+    # print(len(message_bytes))
+    # print(req_values['message_size'])
+    # print(int.from_bytes(data[0:4], byteorder="big"))
+    req_len = req_values['message_size'].to_bytes(4, byteorder="big", signed=True)
     response = req_len + message_bytes
     conn.sendall(response)
-    # conn.close()
+    conn.close()
 
 
 if __name__ == "__main__":
