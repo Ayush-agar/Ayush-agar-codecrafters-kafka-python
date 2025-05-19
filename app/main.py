8dcd3943aaa7dc66fb5257810a3305ab188ecbe9
ayush <ayush.agarwal@spinny.com>
2025-05-19 13:50:51 +0530

    moving on to NH4

--- app/main.py
+++ app/main.py
@@ -1,37 +1,72 @@
-import socket  # noqa: F401
-from traceback import format_exc
-
-
+import socket
+NO_ERROR = 0
+class RequestValidationException(Exception):
+    code: int
+    message: str
+class ApiVersionInvalidException(RequestValidationException):
+    code = 35
+    message = "UNSUPPORTED_VERSION"
+class Request:
+    request_api_key: int
+    request_api_version: int
+    correlation_id: int
+    client_id: str | None
+    tagged_fields: list[str] | None
+    def __init__(
+        self,
+        request_api_key: int,
+        request_api_version: int,
+        correlation_id: int,
+        client_id: str,
+    ):
+        self.request_api_key = request_api_key
+        self.request_api_version = request_api_version
+        self.correlation_id = correlation_id
+        self.client_id = client_id
+    def validate(self):
+        if self.request_api_version not in [0, 1, 2, 3, 4]:
+            raise ApiVersionInvalidException
+def parse_request_length(header: bytes) -> int:
+    return int.from_bytes(header, byteorder="big", signed=True)
+def parse_request(request: bytes) -> Request:
+    request_api_key = int.from_bytes(request[:2], byteorder="big", signed=True)
+    request_api_version = int.from_bytes(request[2:4], byteorder="big", signed=True)
+    correlation_id = int.from_bytes(request[4:8], byteorder="big", signed=True)
+    client_id = bytes.decode(request[8:], "utf-8")
+    return Request(request_api_key, request_api_version, correlation_id, client_id)
+def create_response(request: Request) -> bytes:
+    message_bytes = request.correlation_id.to_bytes(4, byteorder="big", signed=True)
+    min_version, max_version = 0, 4
+    throttle_time_ms = 0
+    tag_buffer = b"\x00"
+    try:
+        request.validate()
+        error_bytes = NO_ERROR.to_bytes(2, byteorder="big", signed=True)
+    except RequestValidationException as ex:
+        error_bytes = ex.code.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += error_bytes
+    message_bytes += int(2).to_bytes(1, byteorder="big", signed=True)
+    message_bytes += request.request_api_key.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += min_version.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += max_version.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += tag_buffer
+    message_bytes += throttle_time_ms.to_bytes(4, byteorder="big", signed=True)
+    message_bytes += tag_buffer
+    req_len = len(message_bytes).to_bytes(4, byteorder="big", signed=True)
+    response = req_len + message_bytes
+    return response
 def main():
-    print("Logs from your program will appear here!")
     with socket.create_server(("localhost", 9092), reuse_port=True) as server:
+        client_conn, addr = server.accept()
         while True:
-            conn, raddr = server.accept() # wait for client
-            message_len = int.from_bytes(conn.recv(4), byteorder="big", signed=True)
-            print(message_len)
-            data = conn.recv(message_len + 8)
-            print(f"data {data}")
-            request_api_key = int.from_bytes(data[:2], byteorder="big", signed=True)
-            request_api_version = int.from_bytes(data[2:4], byteorder="big", signed=True)
-            correlation_id = int.from_bytes(data[4:8], byteorder="big", signed=True)
-            print("correlation_id, request_api_key====")
-            print(correlation_id, request_api_key)
-            client_id = bytes.decode(data[8:], "utf-8")
-            tag_buffer = b"\x00"
-            # correlation , error code, num_api_keys, api_key, min_version, max_version, tag_buffer, throttle_time_ms, tag_buffer
-            message_bytes = correlation_id.to_bytes(4, byteorder="big", signed=True)
-            message_bytes += int(0).to_bytes(2, byteorder="big", signed=True)
-            message_bytes += int(2).to_bytes(1, byteorder="big", signed=True)
-            message_bytes += int(request_api_key).to_bytes(2, byteorder="big", signed=True)
-            message_bytes += int(0).to_bytes(2, byteorder="big", signed=True)
-            message_bytes += int(4).to_bytes(2, byteorder="big", signed=True)
-            message_bytes += tag_buffer
-            message_bytes += int(0).to_bytes(4, byteorder="big", signed=True)
-            message_bytes += tag_buffer
-            req_len = len(message_bytes).to_bytes(4, byteorder="big", signed=True)
-            response = req_len + message_bytes
-            conn.sendall(response)
-            # conn.close()
+            # client_conn, addr = server.accept()
+            # receive
+            message_len = parse_request_length(client_conn.recv(4))
+            request_bytes = client_conn.recv(message_len + 8)
+            # create response
+            response = create_response(parse_request(request_bytes))
+            # send response
+            client_conn.sendall(response)
 
 if __name__ == "__main__":
     main()
