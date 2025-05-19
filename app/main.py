2821468311969eef6d7ea79c0a849c2367d75964
ayush <ayush.agarwal@spinny.com>
2025-05-19 11:55:32 +0530

    minor changes

--- app/main.py
+++ app/main.py
@@ -28,15 +28,27 @@ def main():
             req_values[k] = value
             offset += v
         print(req_values)
+        request_api_key = int.from_bytes(data[:2], byteorder="big", signed=True)
+        request_api_version = int.from_bytes(data[2:4], byteorder="big", signed=True)
+        correlation_id = int.from_bytes(data[4:8], byteorder="big", signed=True)
+        client_id = bytes.decode(data[8:], "utf-8")
         # correlation_id = struct.unpack_from('>I', buffer=data, offset=8)
         tag_buffer = b"\x00"
         # correlation , error code, num_api_keys, api_key, min_version, max_version, tag_buffer, throttle_time_ms, tag_buffer
-        message_bytes = (req_values['correlation_id']).to_bytes(4, signed=True) + (0).to_bytes(2, signed=True) + (1).to_bytes(1, signed=True) + (18).to_bytes(2, signed=True) + (0).to_bytes(2, signed=True) + (4).to_bytes(2, signed=True) + tag_buffer + (0).to_bytes(4, signed=True) + tag_buffer
+        message_bytes = correlation_id.to_bytes(4, signed=True)
+        message_bytes += int(0).to_bytes(2, signed=True)
+        message_bytes += int(1).to_bytes(1, signed=True)
+        message_bytes += int(request_api_key).to_bytes(2, signed=True)
+        message_bytes += int(0).to_bytes(2, signed=True)
+        message_bytes += int(4).to_bytes(2, signed=True)
+        message_bytes += tag_buffer
+        message_bytes += int(0).to_bytes(4, signed=True)
+        message_bytes += tag_buffer
         print(message_bytes)
         print(len(message_bytes))
         print(req_values['message_size'])
         print(int.from_bytes(data[0:4], byteorder="big"))
-        req_len = req_values['message_size'].to_bytes(4, byteorder="big", signed=True)
+        req_len = len(message_bytes).to_bytes(4, byteorder="big", signed=True)
         response = req_len + message_bytes
         print(response)
         conn.sendall(response)
